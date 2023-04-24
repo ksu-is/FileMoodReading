94a030c2633107b12713241e8559c81495ad54b8
MalikEBailey <122242384+MalikEBailey@users.noreply.github.com>
2023-04-24 08:57:44 -0400

    Implement file reading

---
+++ FileWordFinder.py
@@ -0,0 +1,7 @@
+import os
+
+os.system("curl https://github.com/ksu-is/FileWordFinder/blob/main/Paragraphs.txt -o Paragraphs.txt")
+
+paragraphs = open('Paragraphs.txt', 'r')
+words = paragraphs.readlines()
+print(words)
