d81b8f1b59128db24cc9224b1fb793ecd0058934
MalikEBailey <122242384+MalikEBailey@users.noreply.github.com>
2023-04-29 21:24:25 -0400

    Update FileWordFinder.py

--- FileWordFinder.py
+++ FileWordFinder.py
@@ -13,11 +13,29 @@ for i in paragraphs_contents:
     if i.isalpha():
         words = words + i
     elif i == " ":
+        words = words.lower()
         allwords.append(words)
         words = ""
 
 
+
 inp = input("Please enter a word to find: ")
 
+while inp.isalpha() == False:
+    inp = input("Please enter a word to find: ")
 
+
+count = 0
+found = False
+
+for i in allwords:
+    if i.lower() == inp.lower():
+        found = True
+        count = count + 1
+
+if found == True:
+    print(inp + " has been found\n" + str(count) + " instances of " + inp)
+
+elif found == False:
+    print(inp + "cannot be found")
 
